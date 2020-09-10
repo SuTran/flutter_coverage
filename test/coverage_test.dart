54ea8d4ffef5797d2878f5bff1713c71b6bc208c
Su Tran <trannguyentiensu@gmail.com>
2020-09-10 19:17:22 +0700

    init

---
+++ test/coverage_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_coverage/material_test_widget.dart';
+import 'package:flutter_coverage/main.dart';
+import 'package:flutter_coverage/widgets/circle_container_widget.dart';
+import 'package:flutter_coverage/widgets/__test__/circle_container_widget_test.dart';
+void main() {}
